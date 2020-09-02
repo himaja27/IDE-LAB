dd574d46c6fbf07989b88d95831228d7b2be146f
himaja27 <59106994+himaja27@users.noreply.github.com>
2020-09-02 18:16:37 +0530

    Create step3.cs

---
+++ step3.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var name = "laxma";
+            Console.WriteLine("Hello "+name + "!");
+        }
+    }
+}
+© 2020 GitHub, Inc.
